b7a9f5ed48508cea89c15130906713d3a0db393b
Adam <adamos9070@gmail.com>
2022-11-05 21:46:41 +0100

    bomba

--- index.js
+++ index.js
@@ -10,18 +10,6 @@ function toggle() {
   function off() {
     document.getElementById("image").style.display = "none";
   }
-  //if (document.getElementById("image").style.display == "none") {
-  //  document.getElementById("image").style.display = "block";
-  //}
-  //else {
-  //  setTimeout(function () {
-  //    document.getElementById("image").style.display = "none";
-  //  }, 10000);
-  //}
-  //else {
-  //  document.getElementById("image").style.display = "none";
-  //}
-
   var audio = document.getElementById("audio");
   audio.play();
 }
