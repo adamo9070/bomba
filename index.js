5c482788c5ab5f00d6e2687364b359c11bd2ad9e
adamo9070 <adamos9070@gmail.com>
2022-11-05 15:56:08 +0100

    uklad

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 function play() {
-    var audio = document.getElementById('audio');
-    audio.play();
+  var audio = document.getElementById("audio");
+  audio.play();
 }
-
 function toggle() {
+  if ((document.getElementById("image").style.display = "none")) {
+    document.getElementById("image").style.display = "block";
+    setTimeout(off, 3000);
+  }
+  function off() {
+    document.getElementById("image").style.display = "none";
+  }
+  //if (document.getElementById("image").style.display == "none") {
+  //  document.getElementById("image").style.display = "block";
+  //}
+  //else {
+  //  setTimeout(function () {
+  //    document.getElementById("image").style.display = "none";
+  //  }, 10000);
+  //}
+  //else {
+  //  document.getElementById("image").style.display = "none";
+  //}
 
-    if (document.getElementById('image').style.display == 'none') {    
-        document.getElementById('image').style.display = 'block';
-    }
-    else {
-        document.getElementById('image').style.display = 'none';
-    }
-    
-    var audio = document.getElementById('audio');
-        audio.play();
+  var audio = document.getElementById("audio");
+  audio.play();
 }
-            
